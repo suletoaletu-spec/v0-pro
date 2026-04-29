7df543c948fd205b199a9a3ef80e81e0d3243f15
suletoaletu-spec <suletoaletu@gmail.com>
2026-04-29 04:08:28 +0400

    Delete emailService.ts

--- emailService.ts
+++
@@ -1,7 +0,0 @@
-import { dispatchGlobalSupport } from '../Support Logic'; // Adjust path if needed
-
-// Inside your Globe component:
-onGlobeClick={({ lat, lng }) => {
-  dispatchGlobalSupport(lat, lng);
-  alert(`Support Alert Sent for Location: ${lat}, ${lng}`);
-}}
